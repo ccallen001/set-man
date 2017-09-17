19470019e1b912c4f257af6aa9bcd0120a48cb5a
“Chase Allen” <chase.allen@egov.com>
2017-09-17 15:44:18 -0400

    total time

--- js/renderer.js
+++ js/renderer.js
@@ -7,24 +7,37 @@
 
 const database = firebase.database().ref();
 
-let songs = [],
-	id = 0;
+let id = 0;
+
 database.once('value', d => {
-	songs = d.val() || [];
+	data.songs = d.val() || [];
 
 	// gets greatest songId value and sets it as starting point for adding new ids
-	id = songs.map(song => Number(song.id)).sort().pop() || 0;
+	id = data.songs.map(song => Number(song.id)).sort().pop() || 0;
+
+	calcTotalTime();
 
 	renderSongs();
 });
 
 // ---------------------------------------------------------------------------------------------------------------------
 
+// DATA
+// initialize the data object
+
+var data = {
+	songs: [],
+	totalTime: 0
+};
+
+// ---------------------------------------------------------------------------------------------------------------------
+
 // DOM
 
-const [songName, duration] = document.querySelectorAll('.ui input'),
+const [nameInp, durInp] = document.querySelectorAll('.ui input'),
 	addBtn = document.querySelector('button.add'),
-	songList = document.querySelector('.songs');
+	songListDisp = document.querySelector('.songs'),
+	totalTimeDisp = document.querySelector('.total-time');
 
 // ---------------------------------------------------------------------------------------------------------------------
 
@@ -32,17 +45,17 @@ const [songName, duration] = document.querySelectorAll('.ui input'),
 
 // add
 function addSong() {
-	const name = songName.value || null,
-		dur = duration.value || null;
+	const name = nameInp.value || null,
+		dur = durInp.value || null;
 
-	songs.push({
+	data.songs.push({
 		id: ++id,
 		name: name,
 		dur: dur
 	});
 
-	[songName, duration].forEach(input => input.value = null);
-	songName.focus();
+	[nameInp, durInp].forEach(input => input.value = null);
+	nameInp.focus();
 
 	renderSongs();
 }
@@ -50,11 +63,21 @@ addBtn.addEventListener('click', addSong);
 
 // remove
 function removeSong() {
-	songs = songs.filter(song => {
+	data.songs = data.songs.filter(song => {
 		return song.id !== Number(this.parentElement.dataset.songId);
 	});
 
 	renderSongs();
+}
+
+// calculate total time
+function calcTotalTime() {
+	data.totalTime = data.songs.reduce((a, b) => {
+		b = b.dur.split(':');
+		b = ((Number(b[0]) * 60) || 0) + (Number(b[1]) || 0);
+
+		return a + b;
+	}, 0);
 }
 
 // ---------------------------------------------------------------------------------------------------------------------
@@ -63,23 +86,28 @@ function removeSong() {
 
 function renderSongs() {
 	// send to database
-	database.set(songs || null);
+	database.set(data.songs || null);
 
-	if (songs.map)
-		songList.innerHTML = songs
+	if (data.songs.map)
+		songListDisp.innerHTML = data.songs
 			.map(song => `
-			<div data-song-id="${song.id}">
-				<span
-					class="remove"
-					style="
-						font-weight: bold;
-						cursor: pointer;"
-				>(X)
-				</span>
-				<div class="inline-block">${song.name} - ${song.dur}</div>
-			</div>
+				<div class="song" data-song-id="${song.id}">
+					<span
+						class="remove"
+						style="
+							font-weight: bold;
+							cursor: pointer;
+						"
+					>
+						(X)
+					</span>
+					<div class="inline-block">${song.name} - ${song.dur}</div>
+				</div>
 			`)
 			.join('');
 
 	document.querySelectorAll('.remove').forEach(remove => remove.addEventListener('click', removeSong.bind(remove)));
+
+	calcTotalTime();
+	totalTimeDisp.textContent = data.totalTime;
 }
